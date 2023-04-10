9ae1ed3a8686966a7a635b3c618916722e2d0678
brozozowska <s_rozov@me.com>
2023-04-10 21:47:29 +0300

    Initial Commit

---
+++ LifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by Сергей Розов on 10.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
